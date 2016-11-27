80712461e7339ce9eb03b7d4e2c54ceee2ba8a98
David Korff <davidkorff@gmail.com>
2016-11-27 17:26:21 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,81 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+ theo_color = []
+ peter_color = []
+ lucky = []
+ k = []
+ q = []
+ andrew = []
+ alex = []
+ hash = {}
+ data.each do |info,rest|
+   rest.each do |attribute,names|
+     names.each do |name|
+       if name == "Theo"
+         if info == :color
+           theo_color << attribute.to_s
+           hash[name] = {color: theo_color}
+         elsif info == :gender
+           hash[name][info] = [attribute.to_s]
+         elsif info == :lives
+           hash[name][info] = [attribute.to_s]
+         end
+       elsif name == "Peter Jr."
+         if info == :color
+           peter_color << attribute.to_s
+           hash[name] = {color: peter_color}
+         elsif info == :gender
+           hash[name][info] = [attribute.to_s]
+         elsif info == :lives
+           hash[name][info] = [attribute.to_s]
+         end
+       elsif name == "Lucky"
+         if info == :color
+           lucky << attribute.to_s
+           hash[name] = {color: lucky}
+         elsif info == :gender
+           hash[name][info] = [attribute.to_s]
+         elsif info == :lives
+           hash[name][info] = [attribute.to_s]
+         end
+       elsif name == "Ms. K"
+         if info == :color
+           k << attribute.to_s
+           hash[name] = {color: k}
+         elsif info == :gender
+           hash[name][info] = [attribute.to_s]
+         elsif info == :lives
+           hash[name][info] = [attribute.to_s]
+         end
+       elsif name == "Queenie"
+         if info == :color
+           q << attribute.to_s
+           hash[name] = {color: q}
+         elsif info == :gender
+           hash[name][info] = [attribute.to_s]
+         elsif info == :lives
+           hash[name][info] = [attribute.to_s]
+         end
+       elsif name == "Andrew"
+         if info == :color
+           andrew << attribute.to_s
+           hash[name] = {color: andrew}
+         elsif info == :gender
+           hash[name][info] = [attribute.to_s]
+         elsif info == :lives
+           hash[name][info] = [attribute.to_s]
+         end
+       elsif name == "Alex"
+         if info == :color
+           alex << attribute.to_s
+           hash[name] = {color: alex}
+         elsif info == :gender
+           hash[name][info] = [attribute.to_s]
+         elsif info == :lives
+           hash[name][info] = [attribute.to_s]
+         end
+       end
+     end
+   end
+ end
+ hash
 end
